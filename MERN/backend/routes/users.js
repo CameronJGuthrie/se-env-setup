eb25870755fbb44cd0f25b4f0f0124b6fe942f36
Jerry Kim <jerryrox@live.co.kr>
2020-02-29 21:57:37 +1300

    Update users.js

--- MERN/backend/routes/users.js
+++ MERN/backend/routes/users.js
@@ -2,9 +2,10 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
+    // Performing a conflicting change 1
     User.find()
         .then(users => res.json(users))
-        .catch(err => res.status(400).json('Error' + err));
+        .catch(err => res.status(400).json('Error 11111: ' + err));
 });
 
 router.route('/add').post((req, res) => {
